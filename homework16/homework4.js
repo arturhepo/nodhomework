11323a0a2e1ff7e2828eca448633f50824c4aa02
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-21 16:10:36 +0400

    Delete homework4.js

--- homework16/homework4.js
+++
@@ -1,7 +0,0 @@
-// 4.Modules պապակայում ստեղծել wordsFromString մոդուլ wordsFromString ֆունկցիայով որը տրված նախադասությունից
-//   console-ում  արտածում է այն բառերը որոնք չունեն հատուկ սիմվոլ:
-//   Մոդուլը ներմուծել homework4.js ֆայլում և wordsFromString ֆունկցիավ կանչել որևէ նախադասության համար:
-
-
-const wordsFromString = require('./modules/wordsFromString');
-wordsFromString('kjbk wsfw wef djdg% $$lknl kcbn');
