8ec8bb4f6d78c45e9574b216af09d5278421caaa
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-21 16:10:26 +0400

    Delete homework3.js

--- homework16/homework3.js
+++
@@ -1,9 +0,0 @@
-// 3. Modules պապակայում ստեղծել currentTime մոդուլ currentTime ֆունկցիայով որը արտածում է  կանչելու պահին
-//     ժամը, րոպեն ,վարկյանը և միլիվայրկյանը :
-//     Մոդուլը ներմուծել homework3.js ֆայլում և կանչել currentTime ֆունկցիավ  ու արդյունքը արտածել console-ում:
-
-const currentTime = require('./modules/current');
-console.log(currentTime());
-
-
-
