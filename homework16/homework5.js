3d63b65b13287e6508bd496ec0cfd8b62bedd229
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-22 12:52:11 +0400

    Delete homework5.js

--- homework16/homework5.js
+++
@@ -1,8 +0,0 @@
-// 5.Modules պապակայում ստեղծել charOneSecondOutput մոդուլ charOneSecondOutput ֆունկցիայով որը console-ում  արտածում է
-// ֆունկցիային տրված բառի տառերը 1 վայրկյան պարբերությամբ:
-//
-//     Մոդուլը ներմուծել homework5.js ֆայլում և charOneSecondOutput ֆունկցիա կանչել Ձեր անուն ազգանունից  կազմված
-// բառակապակցության համար :
-
-const charOneSecondOutput = require('./modules/charOneSecondOutput');
-charOneSecondOutput('Artur Hepoyan');
