686c14627e4d1574b5b0eaf09083d9eb60a3b0bc
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-22 12:53:29 +0400

    Delete homework2.js

--- homework17/homework2.js
+++
@@ -1,8 +0,0 @@
-// 2. Գրել ծրագիր որ աշխատացնելիս console-ում կարտածի IPv4 IP հասցեն(os.networkInterfaces()):
-
-const os = require('os');
-const netObj = os.networkInterfaces();
-for(let i in netObj){
-    console.log(netObj[i][1].address);
-    break;
-}
