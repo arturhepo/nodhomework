7b2397433174dccc7433c52c37a9cd614f866696
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-22 12:52:41 +0400

    Delete current.js

--- homework16/modules/current.js
+++
@@ -1,5 +0,0 @@
-const currentTime = function (){
-    const dat = new Date();
-    return dat.getHours() + ' : ' + dat.getMinutes() + ' : ' + dat.getSeconds() + ' : ' + dat.getMilliseconds();
-};
- module.exports = currentTime;
