973183e26a1aabcd848e5fa3c70f70f652c42f3e
arturhepo <80718116+arturhepo@users.noreply.github.com>
2021-03-22 12:51:33 +0400

    Delete homework1.js

--- homework16/homework1.js
+++
@@ -1,7 +0,0 @@
-// Ստեղծել homework1.js ֆայլ որը աշխատեցնելուց հետո __filename գլոբալի  միջոցով console-ում արտածել ֆայլի բացարձակ
-// ճանապարը և  ճանապարհի անվան երկարությունը
-
-const l = __dirname.toString().length
-
-console.log('length: ', l);
-console.log(__filename);
